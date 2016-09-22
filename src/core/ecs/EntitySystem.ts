a75335272eec3e3b734463bd7622a92fbc9285da
sskiswani <sskiswani@gmail.com>
2016-09-22 00:11:56 -0500

    whew think its all done now

--- src/core/ecs/EntitySystem.ts
+++ src/core/ecs/EntitySystem.ts
@@ -1,29 +1,30 @@
-import * as _ from 'lodash'
-import * as EventEmitter from 'eventemitter3'
-import { BitSet } from '../lib'
-import { IEventEmitter } from '../Types'
-import { IEntity } from './Entity'
-import { EntityWorld } from './EntityWorld'
+import { BitSet } from "../lib";
+import { IEntity } from "./Entity";
+import { EntityWorld } from "./EntityWorld";
+import { EventEmitter } from "eventemitter3";
 
-// - - - - - - - - - - - - - - - - - - - - -
-
+/**
+ * Decorator that allows specifying components as required for a system.
+ * @export
+ * @param {...Function[]} args
+ * @returns
+ */
 export function RequireComponents(...args: Function[]) {
     return function <T extends typeof EntitySystem>(ctor: T): T {
         let aspect = [];
 
         args.forEach(ctype => {
-            if ('$id' in ctype.prototype)
-                aspect.push(ctype.prototype['$id']);
+            if ("$id" in ctype.prototype) {
+                aspect.push(ctype.prototype.$id["$id"]);
+            }
         });
 
-        ctor['aspect'] = aspect;
+        ctor["aspect"] = aspect;
 
         // return to overwrite prev ctor
         return ctor;
-    }
+    };
 }
-
-// - - - - - - - - - - - - - - - - - - - - -
 
 export interface IEntitySystem {
     isActive: boolean;
@@ -43,45 +44,30 @@ export interface IEntitySystem {
     remove(entity: IEntity): void;
 }
 
-// - - - - - - - - - - - - - - - - - - - - -
-
 export class EntitySystem extends EventEmitter implements IEntitySystem {
-    isActive = true;
+    public isActive: boolean = true;
 
-    private _entities = new BitSet();
-    private _priority;
+    private _entities: BitSet = new BitSet();
+    private _priority: number;
 
-    constructor({priority = 0, active = true} = {}) {
+    public constructor({priority = 0, active = true} = {}) {
         super();
         this._priority = priority;
         this.isActive = active;
     }
 
     //~ Abstract methods
-    preupdate(dt: number) { }
-    update(dt: number) { }
-    postupdate(dt: number) { }
-    attached(world: EntityWorld) { }
-    detached() { }
+    public preupdate(dt: number) { }
+    public update(dt: number) { }
+    public postupdate(dt: number) { }
+    public attached(world: EntityWorld) { }
+    public detached() { }
 
-    interested(entity: IEntity) {
-        return false;
-    }
+    public interested(entity: IEntity) { return false; }
 
-    insert(entity: IEntity) {
-        this._entities.set(entity.id);
-    }
+    public insert(entity: IEntity) { this._entities.set(entity.id); }
+    public remove(entity: IEntity) { this._entities.clear(entity.id); }
 
-    remove(entity: IEntity) {
-        this._entities.clear(entity.id);
-    }
-
-
-    get priority() {
-        return this._priority;
-    }
-
-    get entities() {
-        return this._entities;
-    }
+    public get priority() { return this._priority; }
+    public get entities() { return this._entities; }
 }
--- src/core/ecs/EntityWorld.ts
+++ src/core/ecs/EntityWorld.ts
@@ -1,40 +1,34 @@
-import * as _ from 'lodash'
-// import * as EventEmitter from 'eventemitter3'
-import { getClassName } from '../Utils'
-import { BitSet } from '../lib'
-import { IEntitySystem } from './EntitySystem'
-import { IEntity } from './Entity'
+import { getClassName } from "../Utils";
+import { IEntity } from "./Entity";
+import { IEntitySystem } from "./EntitySystem";
+import * as _ from "lodash";
 
 // - - - - - - - - - - - - - - - - - - - - -
 
-interface Mapping {
-    [$key: string]: number
-}
+interface IMapping { [$key: string]: number; }
 
 // - - - - - - - - - - - - - - - - - - - - -
 
 export class EntityWorld {
-    private _entityMap: Mapping = {};
-    private _systemMap: Mapping = {};
+    private _entityMap: IMapping = {};
+    private _systemMap: IMapping = {};
 
     private _systems: IEntitySystem[] = [];
     private _entities: IEntity[] = [];
 
     private _initialized: boolean = false;
 
-    get entities() { return this._entities; }
-    get systems() { return this._systems; }
+    public get entities() { return this._entities; }
+    public get systems() { return this._systems; }
 
-    constructor() { }
-
-    initialize() {
+    public initialize() {
         this._initialized = true;
         this._entities.forEach(this.insertEntity, this);
         this._systems.sort((a, b) => a.priority - b.priority);
     }
 
-    update(dt: number = -1) {
-        if (!this._initialized) return;
+    public update(dt = -1) {
+        if (!this._initialized) { return; }
 
         this._systems.forEach((system) => {
             if (system.isActive) {
@@ -50,17 +44,15 @@ export class EntityWorld {
     private _notifySystems(entity: IEntity) {
         this._systems.forEach((system) => {
             if (system.entities.get(entity.id)) {
-                if (!system.interested(entity))
-                    system.remove(entity);
+                if (!system.interested(entity)) { system.remove(entity); }
                 return;
             } else {
-                if (system.interested(entity))
-                    system.insert(entity);
+                if (system.interested(entity)) { system.insert(entity); }
             }
         });
     }
 
-    registerSystem(system: IEntitySystem) {
+    public registerSystem(system: IEntitySystem) {
         this._systemMap[getClassName(system)] = this._systems.length;
         this._systems.push(system);
         system.attached(this);
@@ -70,7 +62,7 @@ export class EntityWorld {
         }
     }
 
-    detachSystem(system: IEntitySystem | string) {
+    public detachSystem(system: IEntitySystem | string) {
         let key: string = _.isString(system) ? system : getClassName(system);
         let index: number = this._systemMap[key];
 
@@ -85,37 +77,37 @@ export class EntityWorld {
 
     // - - - - - - - - - - - - - - - - - - - - -
     //~ Entity Management
-    insertEntity(entity: IEntity) {
+    public insertEntity(entity: IEntity) {
         entity.attach(this._entities.length, this);
         this._entities.push(entity);
 
-        entity.on('change', this._notifySystems.bind(this));
+        entity.on("change", this._notifySystems.bind(this));
 
-        if (!this._initialized) return;
+        if (!this._initialized) { return; }
         this._notifySystems(entity);
     }
 
-    getEntity(identifier: number | string): IEntity {
+    public getEntity(identifier: number | string): IEntity {
         if (_.isNumber(identifier)) {
             return this._entities[identifier];
         } else if (_.isString(identifier)) {
             return this._entities[this._entityMap[identifier]];
         }
 
-        console.assert(false, 'shouldnt get here', identifier);
+        console.assert(false, "shouldnt get here", identifier);
     }
 
-    destroyEntity(entity: IEntity | number | string) {
+    public destroyEntity(entity: IEntity | number | string) {
         let e: IEntity;
 
         if (_.isNumber(entity)) {
             e = this._entities[entity];
         } else if (_.isString(entity)) {
             e = this._entities[this._entityMap[entity]];
-        } else if ('id' in entity) {
+        } else if ("id" in entity) {
             e = this._entities[entity.id];
         } else {
-            console.assert(false, 'shouldnt get here', entity);
+            console.assert(false, "shouldnt get here", entity);
         }
 
         e.reset();
