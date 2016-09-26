a6332d951ff4c436f03bac6898bae698a0a399d8
sskiswani <sskiswani@gmail.com>
2016-09-26 08:07:03 -0500

    make aspect usuable

--- src/core/ecs/Aspect.ts
+++ src/core/ecs/Aspect.ts
@@ -1,12 +1,32 @@
+import { IComponent } from "./";
 import { Entity } from "./Entity";
 import { BitSet } from "../lib";
+import * as EventEmitter from "eventemitter3";
 
-export class Aspect {
+export class Aspect{
     private _all: BitSet = new BitSet();
     private _none: BitSet = new BitSet();
     private _one: BitSet = new BitSet();
 
     get empty() { return this._all.isEmpty() && this._none.isEmpty() && this._one.isEmpty(); }
+
+    public constructor(allOf?: IComponent[], noneOf?: IComponent[], oneOf?: IComponent[]) {
+        if (allOf) { allOf.forEach(comp => this._all.set(comp.$id)); }
+        if (noneOf) { noneOf.forEach(comp => this._none.set(comp.$id)); }
+        if (oneOf) { oneOf.forEach(comp => this._one.set(comp.$id)); }
+    }
+
+    public allOf(...components: IComponent[]) {
+        components.forEach(comp => this._all.set(comp.$id));
+    }
+
+    public oneOf(...components: IComponent[]) {
+        components.forEach(comp => this._one.set(comp.$id));
+    }
+
+    public noneOf(...components: IComponent[]) {
+        components.forEach(comp => this._none.set(comp.$id));
+    }
 
     public check(entity: Entity) {
         let ebits = entity.bits;
